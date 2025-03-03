ebb9694ae52a659a2f4013ab83645fb55f67c8fd
MKonrad-afk <s29778@pjwst.edu.pl>
2025-03-03 11:29:42 +0100

    array avrage

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // first
+int[] num = { 1, 2, 3, 4, 5 };
 // second
+     double doSomething(int[] array)
+    {
+       return array[0];
+    }
+
+
+Console.WriteLine(doSomething(num));
 // third
 // fourth
 // fifth
