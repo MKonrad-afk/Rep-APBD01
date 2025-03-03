b33a20f4e7a94679e286cdc2a6ae5c1d49dd2b97
MKonrad-afk <s29778@pjwst.edu.pl>
2025-03-03 11:13:08 +0100

    fifth comment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-// nope 
-//nope 
-//nope 
-//nope 
+// first
+// second
+// third
+// fourth
+// fifth
